12f029c905a77879a9512ff22ac9952923c70686
annibuliful <lagmanmc@gmail.com>
2025-11-29 16:14:12 +0700

    chore: log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebhookController } from './modules/webhook/webhook.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, WebhookController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
